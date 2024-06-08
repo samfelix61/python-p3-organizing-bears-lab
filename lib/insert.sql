cfcb4670f4df697a209b99a498dd63e73e6829a3
samfelix61 <samfelix61@gmail.com>
2024-06-08 11:47:05 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 9, "M", "blue", "mine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 13, "M", "blue", "maine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 14, "F", "green", "maya", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 8, "F", "orange", "main", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 13, "M", "blue", "mvp", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 17, "M", "purple", "money", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 5, "F", "gray", "guys", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 10, "M", "red", "nope", 0);
